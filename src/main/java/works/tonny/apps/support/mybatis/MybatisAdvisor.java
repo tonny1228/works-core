86647665c7ca45b2759326b2344ff29be7785e68
tonny1228 <tonny1228@163.com>
2016-04-20 13:05:29 +0800

    mybatis support

---
+++ src/main/java/works/tonny/apps/support/mybatis/MybatisAdvisor.java
@@ -0,0 +1,16 @@
+package works.tonny.apps.support.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.aop.support.DefaultIntroductionAdvisor;
+
+/**
+ * Mybatis advisor 拦截mybatis支持dao中的方法，使用mybatis进行处理。
+ * Created by Tonny on 2016/4/16.
+ */
+public class MybatisAdvisor extends DefaultIntroductionAdvisor {
+    public MybatisAdvisor(SqlSessionFactory sqlSessionFactory) {
+        super(new MybatisInterceptor(sqlSessionFactory));
+    }
+
+
+}
--- src/test/java/works/tonny/CatalogTest.java
+++ src/test/java/works/tonny/CatalogTest.java
@@ -1,13 +1,21 @@
 package works.tonny;
 
+import org.junit.*;
 import org.llama.library.utils.ThreadLocalMap;
+import org.llama.library.utils.WebAppPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.transaction.annotation.Transactional;
+import works.tonny.apps.core.Catalog;
 import works.tonny.apps.core.CatalogService;
 import works.tonny.apps.user.AuthService;
 import works.tonny.apps.user.LoginedUser;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
 
 /**
  * Created by tonny on 2015/9/9.
@@ -30,13 +38,20 @@ public class CatalogTest extends AbstractJUnit4SpringContextTests {
         LoginedUser admin = authService.login("admin", "1");
         ThreadLocalMap.getInstance().putObject(LoginedUser.class, admin);
         System.out.println("*******************************************************");
-////        List<Catalog> list = catalogService.createQuery().list();
-////        for (Catalog catalog : list) {
-//////            System.out.println(catalog.getTreeNode().getIdLayer());
-////        }
+        List<Catalog> list = catalogService.createQuery().depth(3).list();
+        for (Catalog catalog : list) {
+            System.out.println(catalog.getTreeNode().getIdLayer());
+        }
 //        List<Catalog> cms = catalogService.listSubs("cms");
 //        for (Catalog cm : cms) {
 ////            System.out.println(cm.getName());
 //        }
+    }
+
+
+    @org.junit.Test
+    public void testPath() throws UnsupportedEncodingException {
+        System.out.println(new File(URLDecoder.decode(Thread.class.getResource("/").getPath(), "utf-8")));
+
     }
 }
