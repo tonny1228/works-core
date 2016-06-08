5a6a40ad6a106e586ff586f8d649c4d14a7252f1
tonny1228 <tonny1228@163.com>
2016-06-08 16:19:35 +0800

    兼容以前程序

--- src/main/java/works/tonny/apps/support/EntityLazyProxy.java
+++ src/main/java/works/tonny/apps/support/EntityLazyProxy.java
@@ -1,31 +1,48 @@
 /**
- * 
+ *
  */
 package works.tonny.apps.support;
 
 /**
  * hibernate延迟加载
- * 
+ *
  * @author 祥栋
- * @date 2012-12-18
  * @version 1.0.0
+ * @date 2012-12-18
  */
 public interface EntityLazyProxy {
-	/**
-	 * 刷新一个对象
-	 * 
-	 * @param entity
-	 * @author tonny
-	 */
-    <T> T refresh(Object entity);
+    /**
+     * 刷新一个对象
+     *
+     * @param entity
+     * @author tonny
+     */
+    void refresh(Object entity);
 
-	/**
-	 * 刷新一个对象及其多个属性
-	 * 
-	 * @param entity
-	 * @param fields
-	 * @author tonny
-	 */
-    <T> T refresh(Object entity, String... fields);
+    /**
+     * 刷新一个对象及其多个属性
+     *
+     * @param entity
+     * @param fields
+     * @author tonny
+     */
+    void refresh(Object entity, String... fields);
+
+    /**
+     * 刷新一个对象
+     *
+     * @param entity
+     * @author tonny
+     */
+    <T> T refreshObject(Object entity);
+
+    /**
+     * 刷新一个对象及其多个属性
+     *
+     * @param entity
+     * @param fields
+     * @author tonny
+     */
+    <T> T refreshObject(Object entity, String... fields);
 
 }
--- src/main/java/works/tonny/apps/support/EntityLazyProxyImpl.java
+++ src/main/java/works/tonny/apps/support/EntityLazyProxyImpl.java
@@ -9,7 +9,6 @@ import org.hibernate.collection.AbstractPersistentCollection;
 import org.hibernate.proxy.HibernateProxy;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 import works.tonny.apps.AbstractService;
 
 /**
@@ -27,7 +26,7 @@ public class EntityLazyProxyImpl extends AbstractService implements EntityLazyPr
      */
     @SuppressWarnings("unchecked")
     @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-    public <T> T refresh(Object entity) {
+    public <T> T refreshObject(Object entity) {
         if (entity instanceof AbstractPersistentCollection) {
             AbstractPersistentCollection collection = (AbstractPersistentCollection) entity;
             if (collection.wasInitialized()) {
@@ -58,7 +57,7 @@ public class EntityLazyProxyImpl extends AbstractService implements EntityLazyPr
      */
     @Override
     @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-    public <T> T refresh(Object entity, String... fields) {
+    public <T> T refreshObject(Object entity, String... fields) {
         if (entity instanceof AbstractPersistentCollection) {
             throw new IllegalArgumentException(entity.getClass() + "不允许访问fields");
         }
@@ -76,6 +75,62 @@ public class EntityLazyProxyImpl extends AbstractService implements EntityLazyPr
         }
         entity.toString();
         return (T) entity;
+    }
+
+    /**
+     * @see works.tonny.support.zxtx.apps.ssh.EntityLazyProxy#lookup(java.io.Serializable,
+     * java.lang.String)
+     */
+    @SuppressWarnings("unchecked")
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public void refresh(Object entity) {
+        if (entity instanceof AbstractPersistentCollection) {
+            AbstractPersistentCollection collection = (AbstractPersistentCollection) entity;
+            if (collection.wasInitialized()) {
+                return;
+            }
+            dao.refresh(collection.getOwner());
+            try {
+                final String field = StringUtils.substringAfterLast(collection.getRole(), ".");
+                AbstractPersistentCollection target = (AbstractPersistentCollection) PropertyUtils.getProperty(
+                        collection.getOwner(), field);
+                PropertyUtils.setProperty(collection.getOwner(), field, target);
+                target.toString();
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
+
+        if ((entity instanceof HibernateProxy)
+                && ((HibernateProxy) entity).getHibernateLazyInitializer().isUninitialized()) {
+            dao.refresh(entity);
+            entity.toString();
+        }
+    }
+
+    /**
+     * @see com.zxtx.apps.support.EntityLazyProxy#refresh(java.lang.Object,
+     * java.lang.String[])
+     */
+    @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public void refresh(Object entity, String... fields) {
+        if (entity instanceof AbstractPersistentCollection) {
+            throw new IllegalArgumentException(entity.getClass() + "不允许访问fields");
+        }
+        if ((entity instanceof HibernateProxy)
+                && ((HibernateProxy) entity).getHibernateLazyInitializer().isUninitialized()) {
+            dao.refresh(entity);
+        }
+        for (String field : fields) {
+            try {
+                final Object property = PropertyUtils.getProperty(entity, field);
+                refresh(property);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+        entity.toString();
     }
 
     public BaseDAOSupport getDao() {
